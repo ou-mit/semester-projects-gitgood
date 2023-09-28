f6b7787f51844ca622e13dfa163288449a994466
[Andrew Carbajal] <[andrew.carbajal-1@ou.edu]>
2023-09-28 00:30:41 -0500

    ?

--- TACalender/TACalender/obj/Debug/net6.0-windows/AdminPassword.g.i.cs
+++ TACalender/TACalender/obj/Debug/net6.0-windows/AdminPassword.g.i.cs
@@ -109,7 +109,7 @@ namespace TACalender {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TACalender;component/adminpassword.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TACalender;V1.0.0.0;component/adminpassword.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\AdminPassword.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
